f330f9adc50a567135e74020c5b77c68a609826a
EnigmaCode <72094665+purcidma@users.noreply.github.com>
2021-06-17 15:15:09 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 # Configure the VMware vSphere Provider
 provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
+  user           = "cpoc"
+  password       = "Cisco123!"
+  vsphere_server = "10.88.166.72"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
